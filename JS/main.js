4c41f6bbe51405065e55a2be7d7339d8381377f7
eldrilexjaja12 <diegomuriel2006@cesanrafael.org>
2024-11-19 14:24:48 -0400

    salchipapa30000000000000000000000000

---
+++ JS/main.js
@@ -0,0 +1,21 @@
+let mensaje="hola mundo";
+console.log(mensaje);
+let numero1= 14;
+let numero2= 8;
+let numoero3= 10;
+let resultadosuma= numero1+numero2+numoero3;
+console.log("la suma es " , resultadosuma);
+let resultadoresta= numero1-numero2-numoero3;
+console.log("la resta es " , resultadoresta);
+let resultadomultiplicacion= numero1*numero2*numoero3;
+console.log("la multiplicacion es " , resultadomultiplicacion);
+let resultadodivision= numero1/numero2/numoero3;
+console.log("la division es " , resultadodivision);
+function sumar(){
+    let numero1= 2;
+    let numero2= 3;
+    let numero3= 6;
+    let resultado= numero1+numero2+numero3;
+    alert("la suma es " + resultado);
+}
+
