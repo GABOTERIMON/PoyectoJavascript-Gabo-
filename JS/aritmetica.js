50859f219aca48318c4d9eae9cdfbb4335f9f534
eldrilexjaja12 <diegomuriel2006@cesanrafael.org>
2024-11-26 14:14:02 -0400

    papichulo

--- JS/aritmetica.js
+++ JS/aritmetica.js
@@ -10,3 +10,39 @@ function sumar() {
     resultadoTotal.innerHTML="el resultado es :" + resultadosuma;
     }
 }
+function multiplicación() {
+    let valor1=document.getElementById("dato1").value;
+    let valor2=document.getElementById("dato2").value;
+    let resultadoTotal=document.getElementById("resultado");
+    let resultadosuma=parseFloat(valor1)*parseFloat(valor2);
+    if(valor1=="" || valor2==""){
+        alert("El campo esta vacio");
+    }
+    else{
+    resultadoTotal.innerHTML="el resultado es :" + resultadosuma;
+    }
+}
+function division() {
+    let valor1=document.getElementById("dato1").value;
+    let valor2=document.getElementById("dato2").value;
+    let resultadoTotal=document.getElementById("resultado");
+    let resultadosuma=parseFloat(valor1)/parseFloat(valor2);
+    if(valor1=="" || valor2==""){
+        alert("El campo esta vacio");
+    }
+    else{
+    resultadoTotal.innerHTML="el resultado es :" + resultadosuma;
+    }
+}
+function resta() {
+    let valor1=document.getElementById("dato1").value;
+    let valor2=document.getElementById("dato2").value;
+    let resultadoTotal=document.getElementById("resultado");
+    let resultadosuma=parseFloat(valor1)-parseFloat(valor2);
+    if(valor1=="" || valor2==""){
+        alert("El campo esta vacio");
+    }
+    else{
+    resultadoTotal.innerHTML="el resultado es :" + resultadosuma;
+    }
+}
